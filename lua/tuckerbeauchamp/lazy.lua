947bd5b70ed6b2b2ee3c1c10448b1fc20274a8e0
Tucker Beauchamp <tuckerbeauchamp@gmail.com>
2023-11-10 13:18:37 -0700

    add copilot to lazy

--- lua/tuckerbeauchamp/lazy.lua
+++ lua/tuckerbeauchamp/lazy.lua
@@ -5,14 +5,14 @@
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 
 vim.opt.rtp:prepend(lazypath)
@@ -30,68 +30,71 @@ return require('lazy').setup({
         }
     },
 
-  -- Keep current context above cursor
-  "nvim-treesitter/nvim-treesitter-context",
+    -- Copilot
+    "github/copilot.vim",
 
-  -- Undotree to keep undos stored for later use
-  "mbbill/undotree",
+    -- Keep current context above cursor
+    "nvim-treesitter/nvim-treesitter-context",
 
-  -- Formatter that respects prettier
-  "sbdchd/neoformat",
+    -- Undotree to keep undos stored for later use
+    "mbbill/undotree",
 
-  -- Comment out line of text
-  "tpope/vim-commentary",
+    -- Formatter that respects prettier
+    "sbdchd/neoformat",
 
-  -- Allow text wrapping based off symbol
-  "tpope/vim-surround",
+    -- Comment out line of text
+    "tpope/vim-commentary",
 
-  -- Linter
-  "mfussenegger/nvim-lint",
+    -- Allow text wrapping based off symbol
+    "tpope/vim-surround",
 
-  -- web dev icons for below packages. trying to prevent depency issue
-  'nvim-tree/nvim-web-devicons',
+    -- Linter
+    "mfussenegger/nvim-lint",
 
-  -- Fuzzy Finder
-  {
-      'nvim-telescope/telescope.nvim', tag = '0.1.4',
-      -- or                            , branch = '0.1.x',
-      dependencies = { 'nvim-lua/plenary.nvim' }
-  },
+    -- web dev icons for below packages. trying to prevent depency issue
+    'nvim-tree/nvim-web-devicons',
 
-  {
-      "catppuccin/nvim",
-      name = "catppuccin",
-      config = function()
-          vim.cmd('colorscheme catppuccin')
-      end
-  },
+    -- Fuzzy Finder
+    {
+        'nvim-telescope/telescope.nvim', tag = '0.1.4',
+        -- or                            , branch = '0.1.x',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    },
 
-  -- Language server managed by Mason
-  {
-	  'VonHeikemen/lsp-zero.nvim',
-	  branch = 'v3.x',
-	  dependencies = {
-		  --- Uncomment these if you want to manage LSP servers from neovim
-		  'williamboman/mason.nvim',
-		  'williamboman/mason-lspconfig.nvim',
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        config = function()
+            vim.cmd('colorscheme catppuccin')
+        end
+    },
 
-          -- LSP Support
-          'neovim/nvim-lspconfig',
-          -- Autocompletion
-          'hrsh7th/nvim-cmp',
-          'hrsh7th/cmp-nvim-lsp',
-          'L3MON4D3/LuaSnip',
-      }
-  },
+    -- Language server managed by Mason
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v3.x',
+        dependencies = {
+            --- Uncomment these if you want to manage LSP servers from neovim
+            'williamboman/mason.nvim',
+            'williamboman/mason-lspconfig.nvim',
 
-  -- Auto create pairs ()/{} etc..
-  {
-      "windwp/nvim-autopairs",
-      config = function() require("nvim-autopairs").setup {} end
-  },
-  -- Status Line
-  {
-      'nvim-lualine/lualine.nvim',
-      dependencies = { 'nvim-tree/nvim-web-devicons' }
-  },
+            -- LSP Support
+            'neovim/nvim-lspconfig',
+            -- Autocompletion
+            'hrsh7th/nvim-cmp',
+            'hrsh7th/cmp-nvim-lsp',
+            'L3MON4D3/LuaSnip',
+        }
+    },
+
+    -- Auto create pairs ()/{} etc..
+    {
+        "windwp/nvim-autopairs",
+        config = function() require("nvim-autopairs").setup {} end
+    },
+    -- Status Line
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' }
+    },
 })
